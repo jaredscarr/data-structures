f063893573ab7877aa181a10527a45879b2cbce6
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-03-09 23:11:12 -0800

    Add tests for node and list classes as well as insert. Wrote half of append.

--- dll.py
+++ dll.py
@@ -1,1 +1,108 @@
 # -*- coding: utf-8 -*-
+
+
+class Node(object):
+    """Construct Node object."""
+
+    def __init__(self, val):
+        """Initialize node object."""
+        self.val = val
+        self.next = None
+        self.previous = None
+
+
+class DoublyLinkedList(object):
+    """Handle creation of a linked list."""
+
+    def __init__(self, iterable=None):
+        """Init linked list object with optioanl itterable."""
+        self.head = None
+        self.iterable = iterable
+        if iterable:
+            try:
+                for i in iterable:
+                    self.insert(i)
+            except TypeError:
+                print('value is not an interable')
+
+    def insert(self, val):
+        """Insert new node at head of list."""
+        new_node = Node(val)
+        if new_node.next is None:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            next_node = new_node.next
+            next_node.previous = new_node
+            self.head = new_node
+
+
+
+
+
+
+    # def search(self, val):
+    #     """Search for value and return node."""
+    #     current = self.head
+    #     found = False
+    #     while current and not found:
+    #         if current.val == val:
+    #             found = True
+    #             return current.val
+    #         current = current.next
+    #     return None
+
+    # def pop(self):
+    #     """Remove node at head and returns the value."""
+    #     current = self.head
+    #     new_head = current.next
+    #     new_head = self.head
+    #     return current.val
+
+    # def remove(self, val):
+    #     """Remove specific val."""
+    #     current = self.head
+    #     previous = None
+    #     found = False
+    #     while current is not None and not found:
+    #         if current.val == val:
+    #             previous = current
+    #             current = current.next
+    #             found = True
+    #         else:
+    #             current = current.next
+    #             previous = current.val
+    #         if current is None:
+    #             raise ValueError('Node not in list!')
+    #         if previous is None:
+    #             self.head = current.next
+    #         else:
+    #             previous.next = current.next
+
+    # def size(self):
+    #     """Return length of list."""
+    #     current = self.head
+    #     counter = 0
+    #     while current is not None:
+    #         counter += 1
+    #         current = current.next
+    #     return counter
+
+    def display(self):
+        """Print list as tuple."""
+        container = []
+        current = self.head
+        while current is not None:
+            container.append(current.val)
+            current = current.next
+        print(tuple(container))
+        return tuple(container)
+
+
+
+
+
+
+
+
+
--- test_dll.py
+++ test_dll.py
@@ -1,1 +1,47 @@
 # -*- coding: utf-8 -*-
+
+
+def test_node():
+    """Assert node class is created with specified val."""
+    from dll import Node
+    new_node = Node(4)
+    assert new_node.val == 4
+
+
+def test_double_linked_list():
+    """Assert a doubly linked list is created."""
+    from dll import DoublyLinkedList
+    my_list = DoublyLinkedList()
+    assert my_list.head is None
+
+
+def test_insert_on_empty_list():
+    """Assert insert works."""
+    from dll import DoublyLinkedList
+    my_list = DoublyLinkedList()
+    my_list.insert(4)
+    assert my_list.head.val == 4
+
+
+def test_insert_on_non_emtpy_list():
+    """Assert insert on non-empty list works."""
+    from dll import DoublyLinkedList
+    my_list = DoublyLinkedList()
+    my_list.insert(4)
+    my_list.insert('Diana')
+    assert my_list.head.val == 'Diana'
+
+
+def test_assert_previous():
+    """Assert second node points to its previous."""
+    from dll import DoublyLinkedList
+    my_list = DoublyLinkedList()
+    my_list.insert('Daniel')
+    my_list.insert('Diana')
+    assert my_list.head.next == 'Daniel'
+
+
+
+
+
+
