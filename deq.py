5bb5babb13fc113825feda62cc43e43c93038b81
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-03-14 13:57:50 -0700

    Add append and append left methods/tests and pop

---
+++ deq.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""Implementation of the deque class."""
+
+from dll import DoublyLinkedList
+
+
+class Deque(object):
+    """Deque class."""
+
+    def __init__(self):
+        """Initilizer of deque class."""
+        self.container = DoublyLinkedList()
+
+    def append_left(self, val):
+        """Append val to the head of the list."""
+        self.container.insert(val)
+
+    def append(self, val):
+        """Append val to the tail of the list."""
+        self.container.append(val)
+
+    def pop(self):
+        """Return and remove head from the list."""
+        self.container.pop()
+
+
---
+++ test_deq.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""Test methods of the deque class."""
+import pytest
+
+
+def test_append_left_on_empty():
+    """Assert append_left adds node to the head."""
+    from deq import Deque
+    my_list = Deque()
+    my_list.append_left(7)
+    assert my_list.container.head.val == 7
+
+
+def test_append_left_non_empty():
+    """Assert append left adds node to the head."""
+    from deq import Deque
+    my_list = Deque()
+    my_list.append_left(7)
+    my_list.append_left('thiat')
+    assert my_list.container.head.val == 'thiat'
+
+
+def test_append_empty():
+    """Assert append adds node to the tail."""
+    from deq import Deque
+    my_list = Deque()
+    my_list.append('thiat')
+    assert my_list.container.tail.val == 'thiat'
+
+
+def test_append_non_empty():
+    """Assert append adds node to the tail."""
+    from deq import Deque
+    my_list = Deque()
+    my_list.append('thies')
+
+
+def test_pop_empty():
+    """Assert raises error."""
+    from deq import Deque
+    with pytest.raises(AttributeError):
+        my_list = Deque()
+        my_list.Deque.pop()
+
+
+def test_pop():
+    """Assert pops val from the head."""
+    from deq import Deque
+    my_list = Deque()
+    my_list.append_left('thies')
+    my_list.append_left('thiat')
+    assert my_list.pop() == 'thiat'
+
+
+
+
