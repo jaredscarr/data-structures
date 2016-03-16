004b32ce065649a59e0aa05f7687d5b6ad8ece1b
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-03-16 14:46:09 -0700

    Something is working?

--- binheap.py
+++ binheap.py
@@ -4,26 +4,77 @@ class Heap(object):
 
     def __init__(self, iterable=None):
         """Initialize Heap."""
-        if iterable:
-            self.container = list(iterable)
-            self.container.insert(0, '')
-        else:
-            self.container = ['']
+        # if iterable:
+        #     self.container = list(iterable)
+        #     for i in self.container:
+        #         self.push(i)
+        # else:
+        self.container = ['a','b', 'c', 'd']
 
     def left(self, index):
         """Return left child of parent."""
-        return self.container[index * 2]
+        return self.container[(index * 2) + 1]
 
     def right(self, index):
         """Return right child of parent."""
-        return self.container[index * 2 + 1]
+        return self.container[(index * 2) + 2]
 
     def parent(self, index):
         """Return parent of a child."""
-        return self.container[index // 2]
+        return self.container[index // 2 - 1]
 
-    # def heapify(self):
 
-    # def pop(self):
 
-    # def push(self):
+# def swappy_thing(self, child, parent):
+#     while self.root < startpos:
+#         temp = self.container[child]
+#         self.container[parent] = self.container[child]
+#         self.container[parent] = temp
+
+
+#     def bubble_up():
+#         swappy_thing(left(index), parent(index))
+
+
+
+
+
+
+
+
+
+
+
+
+#     @property
+#     # to set length of list
+#     def foo(self):
+#         return self._foo
+
+# # Use len - 1, not insert ''
+
+
+#     # def heapify(self):
+
+#     # def pop(self):
+
+#     # def push(self):
+
+
+
+
+#     def swap(index):
+#         temp = my_list[parent(index)]
+#         my_list[parent(index)] = my_list[index]
+#         my_list[index] = temp
+
+#     while (index+1) // 2 > 0:
+
+
+#         parent = (index - 1) >> 1
+#         bitshifting
+
+
+
+
+
--- test_binheap.py
+++ test_binheap.py
@@ -2,29 +2,29 @@
 def test_iterable_passed():
     """Test if Heap is initialized."""
     from binheap import Heap
-    my_heap = Heap([1, 2, 3, 4]) 
-    assert my_heap.container[0] == ''
+    my_heap = Heap([1, 2, 3, 4])
+    assert my_heap.container[0] == 'a'
 
 
 def test_left():
     """Assert if item in list is the left child."""
     from binheap import Heap
-    my_heap = Heap([1, 2, 3, 4])
-    assert my_heap.left(1) == 2
+    my_heap = Heap(['a', 'b', 'c', 'd'])
+    assert my_heap.left(0) == 'b'
 
 
 def test_right():
     """Assert if item in list is the right child."""
     from binheap import Heap
     my_heap = Heap([1, 2, 3, 4])
-    assert my_heap.right(1) == 3
+    assert my_heap.right(0) == 'c'
 
 
 def test_parent():
     """Assert if the item in the list is a parent."""
     from binheap import Heap
     my_heap = Heap([1, 2, 3, 4])
-    assert my_heap.parent(2) == 1
+    assert my_heap.parent(3) == 'a'
 
 # def test_parent():
 
