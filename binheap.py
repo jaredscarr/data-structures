a906044279cb8326696c6d580a79aad93fc3c2cf
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-03-16 15:51:03 -0700

    added self in front of methods inside push.

--- binheap.py
+++ binheap.py
@@ -23,26 +23,22 @@ class Heap(object):
         """Return parent of a child."""
         return self.container[index // 2 - 1]
 
-     def swappy(self, value):
+    def swappy(self, value):
+        """Swap parent and child."""
         val_index = self.container.index(value)
         temp_location = self.container[val_index]
-        current_parent = parent(val_index)
+        current_parent = self.parent(val_index)
         self.container[current_parent] = value
         self.container[val_index] = temp_location
         return self.container[current_parent]
-
-
-
-        self.container[parent] = self.container[child]
-        self.container[parent] = temp
 
     def push(self, value):
         """Push a value to the Heap and maintain Heap structure."""
         heap = self.container
         heap.append(value)
-        current_parent = parent(value)
+        current_parent = self.parent(value)
         while current_parent < value:
-            current_parent = swappy(value)  
+            current_parent = self.swappy(value)
 
 
 #     def bubble_up():
