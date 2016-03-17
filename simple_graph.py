1996248a6e93b86f0d57ba50d41c4ff6e6772b29
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-03-17 14:36:13 -0700

    Node and Graph objects started.

---
+++ simple_graph.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+class Node(object):
+    """Node."""
+
+    def __init__(self, value):
+        """Init Node."""
+        self.value = value
+
+
+class Graph(object):
+    """The Graph."""
+
+    def __init__(self):
+        """Init Graph."""
+        self.container = {}
+
+    
---
+++ test_simple_graph.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+def test_display():
+    """Test that the graph displays."""
+    from simple_graph import 
