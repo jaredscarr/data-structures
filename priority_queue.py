529134ce10b56d4908ee1eba9d691b5073abbe38
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-03-20 15:03:08 -0700

    Added tests for pop and insert. Code passes.

--- priority_queue.py
+++ priority_queue.py
@@ -4,18 +4,40 @@
 class PriorityQueue(object):
     """A priority Queue."""
 
-
     def __init__(self):
         """Initialize PriorityQueue object."""
-        self.priority_queue = []
+        self.container = []
 
-    def insert(self, value, priority=3):
-        """Add value and priority to priority queue."""
-        self.priority_queue.push
+    def insert(self, item):
+        """Insert item into the queue."""
+        self.container.append(item)
+        child = len(self.container) - 1
+        parent = (child - 1) // 2
+        while child > 1 and self.container[child - 1][0] > self.container[parent - 1][0]:
+            small_parent = self.container[parent - 1]
+            self.container[parent - 1][0] = self.container[child - 1][0]
+            self.container[child - 1][0] = small_parent
+            child = parent
+            parent = (child - 1) // 2
 
-    # def pop(self):
-    #     """Pop the highest priority."""
-    #     self.priority_queue
+    def pop(self):
+        """Pop the highest priority in the queue."""
+        self.container[0] = self.container.pop()
+        self.sort_to_leaf(0)
 
-    # def peek(self):
-    #     """Return highest priority."""
+    def sort_to_leaf(self, index):
+        """Sort up the heap."""
+        index += 1
+        left = 2 * index
+        right = 2 * index + 1
+        most_huge = self.container[index][0]
+        size = len(self.container)
+        if left <= size and self.container[left - 1][0] > self.container[most_huge - 1][0]:
+            most_huge = left
+        if right <= left and self.container[right - 1][0] > self.container[most_huge - 1][0]:
+            most_huge = right
+        if most_huge != self.container[index][0]:
+            value = self.container[index - 1]
+            self.container[index - 1] = self.container[most_huge - 1]
+            self.container[most_huge - 1] = value
+            self.sort_to_leaf(most_huge - 1)
--- test_priority_queue.py
+++ test_priority_queue.py
@@ -7,32 +7,15 @@ def test_insert():
     """Test if value is inserted into the queue."""
     from priority_queue import PriorityQueue
     pq = PriorityQueue()
-    pq.priority_queue.append(('value', 1))
-    assert pq.priority_queue[0] == ('value', 1)
-
-# .pop(): removes the most important item from the queue.
-# def test_pop():
-#     """Test if popped item is the top of the queue."""
-#     from priority_queue import PriorityQueue
-#     pq = PriorityQueue()
-#     pq.insert(('value', 1))
-#     assert pop() == ('value', 1)
+    item = (1, 'value')
+    pq.insert(item)
+    assert pq.container[0] == (1, 'value')
 
 
-# def test_pop_removed_item():
-#     """Test if popped item is the top of the queue."""
-#     from priority_queue import PriorityQueue
-#     pq = PriorityQueue()
-#     pq.priority_queue['value'] = 1
-#     pq.priority_queue['second'] = 2
-#     pq.pop()
-#     assert 'value' not in pq.priority_queue
-
-# .peek(): returns the most important item without removing it from the queue.
-# def test_peek():
-#     """Test if the most important is returned but still in queue."""
-#     from priority_queue import PriorityQueue
-#     pq = PriorityQueue()
-#     pq.priority_queue['value'] = 1
-#     pq.priority_queue['second'] = 2
-#     assert peek() == 1
+def test_pop():
+    """Test if removes highest priority."""
+    from priority_queue import PriorityQueue
+    pq = PriorityQueue()
+    pq.container = [(2, 'fish'), (3, 'sticks'), (4, 'chix'), (10, 'ticks')]
+    pq.pop()
+    assert pq.container == [(10, 'ticks'), (3, 'sticks'), (4, 'chix')]
