374e7962f32c950ca20ad53516b09aab9e773816
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-03-27 13:20:46 -0700

    Fixed the spelling of Daniels last name.

--- setup.py
+++ setup.py
@@ -5,11 +5,10 @@ setup(
     name='data-structures',
     description="Various data-structure assignments",
     version=0.1,
-    author="Daniel Swelling and Jared Scarr",
+    author="Daniel Zwelling and Jared Scarr",
     author_email="jaredscarr@gmail.com",
     license='MIT',
     py_modules=['mail_room'],
-    # package_dir={'': 'mail-room'},
     install_requires=[],
     extras_require={'test': ['pytest', 'pytest-xdist', 'tox']},
 )
